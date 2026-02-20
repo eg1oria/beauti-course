a04ff70ef2eda11db9fdcf898d26cd6eb4ad3768
Eglor <leontevegor57@gmail.com>
2026-02-20 15:01:43 +0500

    dfdf

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://172.20.10.5:3000'],
+    origin: ['http://localhost:3000', 'http://212.19.134.48'],
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
